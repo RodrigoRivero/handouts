cc54b2eb2927abca500d383fa60b52e96c2efdd8
RodrigoRivero <rodrigoriverocastro@gmail.com>
2017-07-18 20:07:09 +0000

    worked on some stuff

--- worksheet-1.R
+++ worksheet-1.R
@@ -1,13 +1,13 @@
 ## The Editor
 
-vals <- ...
+vals <- seq(1,50)
 
-vals <- seq(...,
-            ...)
+vals <- seq(from = 1,
+            to = 50)
 
 ## Vectors
 
-counts ...
+counts <- c( 2, 6, 4, 6)
 
 ## Exercise 2
 
@@ -15,68 +15,83 @@ counts ...
 
 ## Factors
 
-education <- ...(c("college", "highschool", "college", "middle"),
-                 ... = c("middle", "highschool", ...))
+education <- factor(c("college", "highschool", "college", "middle"),
+                 levels = c("middle", "highschool", "college"))
 
-education <- ...(c("college", "highschool", "college", "middle"),
-                 levels = c("middle", "highschool", "college"),
-                 ...)
+education <- factor(
+  c("college", "highschool", "college", "middle"),
+  levels = c("middle", "highschool", "college"),
+  ordered = TRUE)
 
 ## Data Frames
 
-... data.frame(...)
+df <- data.frame(education, counts)
 
 ## Exercise 3
 
-...
+species <- c("polystichum", "asplenium", "octarine")
+abund <- c( 100, 90, 75)
+data <- data.frame(species, abund)
 
 ## Load data into R
 
-plots <- ...(...)
-animals <- ...(...)
+plots <- read.csv('data/plots.csv')
+
 
 ## Exercise 4
 
-...
+animals <- read.csv('data/animals.csv', stringsAsFactors = FALSE, na.strings = '')
+animals$sex <- factor(animals$sex)
 
 ## Names
 
-...(df) <- c(...)
+names(df) <- c("ed", "ct")
 
 ## Subsetting ranges
 
 days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-weekdays <- ...
+weekdays <- days[2:6]
+weekends <- days[c(1,7)]
+days[-4]
 ...
 
 ## Exercise 5
-
-...
-
+?seq
+weekdays <- days[c(-1,-7)]
+mWF <- days[seq(from = 2, to = 6, by = 2)]
 
 ## Exercise 6
+education <- factor(
+  c("college", "highschool", "college", "middle"),
+  levels = c("middle", "highschool", "college"),
+  ordered = TRUE)
+counts <- c(4, 3, 7, 5)
+df <- data.frame(education, counts)
+names(df) <- c('ed', 'ct')
+df[3, 'ed']
+df[c(-1,-4), "ed"]
 
-...
 
 ## Base plotting
 
-plot(..., ...)
+plot(animals$hindfoot_length, animals$weight)
 
 ## Histograms
 
-...(surveys$...)
+hist(animals$weight)
 
 ## Boxplots
 
-boxplot(...)
+boxplot(log(weight)~year, data = animals)
 
 ## Anatomy of a function
 
-function(...) {
-  ...
-  return(...)
+first <- function(dat) {
+  result <- dat[1,]
+  return(result)
 }
-
+first(df)
+first(counts)
 ## Flow control
 
 if (...) {
@@ -85,17 +100,30 @@ if (...) {
     ...
 }
 
-firts <- function(...) {
-    if (...) {
-        ...
+first <- function(dat) {
+    if (is.vector(dat)) {
+        result <- dat[1]
     } else {
-        ...
+        result <- dat[1,1 ]
     }
+  return(result)
 }
 
+first(df)
+first(counts)
 ## Exercise 7
 
-...
+first <- function(dat) {
+  if (is.vector(dat)) {
+    result <- dat[1]
+  } else if(is.matrix(dat)) {
+    result <- dat[1,1]
+  } else {
+    result <- dat[1, ]
+  }
+  return(result)
+}
+
 
 ## Distributions and statistics
 
--- worksheet-2.sh
+++ worksheet-2.sh
@@ -2,13 +2,12 @@
 
 ## Configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name RodrigoRivero
+git config --global user.email rodrigoriverocastro@gmail.com
 
 ## Change the "origin" remote URL and push
-git remote set-url origin ...
-git push --all
+git remote set-url origin git@github.com:RodrigoRivero/handouts.git
 
 ## Set upstream
-#git remote add upstream https://github.com/sesync-ci/handouts
-#git pull upstream master
+git remote add upstream https://github.com/sesync-ci/handouts
+git pull upstream master
